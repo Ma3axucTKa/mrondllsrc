46476d018e7fd56745fa9f8f5f1c0f45f9e3f5f2
Ma3axucTKa <xaba145@mail.ru>
2024-06-19 20:59:10 +0300

    updated watermark
    fixed command to disable watermark

--- hook_lib/screen.cpp
+++ hook_lib/screen.cpp
@@ -7,7 +7,8 @@ void CG_DrawWaterMark()
 	float white[4] = { 1.0f, 1.0f, 1.0f, 0.075f };
 	float red[4] = { 1.0f, 0.0f, 0.0f, 0.5f };
 	float blue[4] = { 0.5f, 0.5f, 1.5f, 1.5f };
-	CL_DrawText(0x14EF2DEA0_g, "MRON S1", 0x7FFFFFFF, *reinterpret_cast<uintptr_t*>(0x14EEB0C68_g), 0, 480, 1, 1, 0.45000001, 0.45000001, blue, 7);
+	float lightblue[4] = {0.7f, 0.7f, 1.0f, 1.0f};
+	CL_DrawText(0x14EF2DEA0_g, "MRON IV", 0x7FFFFFFF, *reinterpret_cast<uintptr_t*>(0x14EEB0C68_g), 0, 480, 1, 1, 0.45000001, 0.45000001, lightblue, 7);
 }
 
 void CL_ScreenMP_DrawOverlay_Detour()
